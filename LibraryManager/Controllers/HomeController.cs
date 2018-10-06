39879f2c3585657f8f41151a3bafba0223d6d584
vernellparker <vernell.parker@gmail.com>
2018-10-06 04:44:56 -0400

    Conncected Database

--- LibraryManager/Controllers/HomeController.cs
+++ LibraryManager/Controllers/HomeController.cs
@@ -8,10 +8,7 @@ namespace LibraryManager.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        public ActionResult Index() => View();
 
         public ActionResult About()
         {
