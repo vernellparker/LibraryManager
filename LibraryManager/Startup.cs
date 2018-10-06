285b786abc3a925a06b5b036e61fa3b037868b0b
vernellparker <vernell.parker@gmail.com>
2018-10-06 03:23:28 -0400

    Add project files.

---
+++ LibraryManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LibraryManager.Startup))]
+namespace LibraryManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
