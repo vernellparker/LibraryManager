33421cf8748caf35f4151c3a75a5302062ddc297
vernellparker <vernell.parker@gmail.com>
2018-10-06 09:10:24 -0400

    Added Genre controller and views

---
+++ LibraryManager/Controllers/GenreController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManager.Models;
+
+namespace LibraryManager.Controllers
+{
+    public class GenreController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public GenreController()
+        {
+            _db = new ApplicationDbContext();
+        }
+        // GET: Genre
+        public ActionResult Index()
+        {
+            return View(_db.Genres.ToList());
+        }
+
+        //GET: Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Genres.Add(genre);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+        }
+    }
+}
--- LibraryManager/Models/Genre.cs
+++ LibraryManager/Models/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,8 @@ namespace LibraryManager.Models
     public class Genre
     {
         [Required] public int Id { get; set; }
-        [Required] public string Name { get; set; }
+        [Required] 
+        [DisplayName("Genre Name")]
+        public string Name { get; set; }
     }
 }
